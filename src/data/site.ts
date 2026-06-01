064fee2cd6079badcfcd360c454762fc16f7f474
sladkaroww <sladkaroww@gmail.com>
2026-06-01 22:27:23 +0300

    Add Source link to GitHub repo in footer

--- src/data/site.ts
+++ src/data/site.ts
@@ -17,4 +17,5 @@ export const youtubeChannel = 'https://www.youtube.com/@schoolpodcast32';
 export const facebookPage = 'https://www.facebook.com/school32.bg/';
 export const instagramPage = 'https://www.instagram.com/32.podcast/';
 export const schoolSite = 'https://school32.com';
+export const githubRepo = 'https://github.com/32-Podcast/www';
 export const contactEmail = 'school32.podcast32@school32.com';
