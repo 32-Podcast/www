c82aed7adca7de4fbc85f5a899cb56dbe5dc55a7
sladkaroww <sladkaroww@gmail.com>
2026-06-01 19:31:17 +0300

    Fix Сладкаров surname and update contact email

--- src/data/site.ts
+++ src/data/site.ts
@@ -17,3 +17,4 @@ export const youtubeChannel = 'https://www.youtube.com/@schoolpodcast32';
 export const facebookPage = 'https://www.facebook.com/school32.bg/';
 export const instagramPage = 'https://www.instagram.com/32.podcast/';
 export const schoolSite = 'https://school32.com';
+export const contactEmail = 'school32.podcast32@school32.com';
