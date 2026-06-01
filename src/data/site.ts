586db01d1a66da6990e61a115b8305207ba938c6
sladkaroww <sladkaroww@gmail.com>
2026-06-01 22:32:24 +0300

    Add GitHub org link to social row in footer

--- src/data/site.ts
+++ src/data/site.ts
@@ -18,4 +18,5 @@ export const facebookPage = 'https://www.facebook.com/school32.bg/';
 export const instagramPage = 'https://www.instagram.com/32.podcast/';
 export const schoolSite = 'https://school32.com';
 export const githubRepo = 'https://github.com/32-Podcast/www';
+export const githubOrg = 'https://github.com/32-Podcast';
 export const contactEmail = 'school32.podcast32@school32.com';
