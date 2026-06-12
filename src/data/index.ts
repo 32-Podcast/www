e87d195d28d33838a0cfb6cda38ade14dc347b9b
Peter Sladkarov <peter.sladkarov@gmail.com>
2026-06-12 14:11:53 +0300

    Fix: Remove problematic index.ts file due to TypeScript verbatimModuleSyntax errors

--- src/data/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export all data exports from a single file for easier imports
-export { episodes, Episode } from './episodes';
-export { team, TeamMember } from './team';
-export { events, Event } from './events';
-export { site } from './site';
