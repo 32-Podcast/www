89caeaf43e3166462bf29b95b2503e618559ad77
Peter Sladkarov <peter.sladkarov@gmail.com>
2026-06-13 17:20:19 +0300

    Fix dates from YouTube: EP.2=2025-11-25, EP.10=2026-03-06, EP.11=2026-05-07

--- src/data/episodes.ts
+++ src/data/episodes.ts
@@ -8,7 +8,7 @@ export interface Episode {
 
 export const episodes: Episode[] = [
   { id: 'LvSwqCOkcvM', order: 1, rubric: 'Междучасие', title: '"Междучасие" с Петър Михайлов', date: '2025-10-17' },
-  { id: 'KuoooDT9_vo', order: 2, rubric: 'SPECIAL', title: 'Патронният празник на нашето училище', date: '2025-10-25' },
+  { id: 'KuoooDT9_vo', order: 2, rubric: 'SPECIAL', title: 'Патронният празник на нашето училище', date: '2025-11-25' },
   { id: 'M9y3vu4m0JA', order: 3, rubric: 'Междучасие', title: '"Междучасие" с Драгомир Маринов', date: '2025-12-19' },
   { id: 'BaYqRiYzN3k', order: 4, rubric: 'SPECIAL', title: 'Кой знае отговора? | Коледен Quiz', date: '2025-12-28' },
   { id: 'BYpvUWqEWNM', order: 5, rubric: 'Междучасие', title: 'Преплитането на две култури | Erasmus+', date: '2026-01-12' },
@@ -16,8 +16,8 @@ export const episodes: Episode[] = [
   { id: 'HC2CXJ5HHjg', order: 7, rubric: 'Междучасие', title: 'Локалите', date: '2026-01-26' },
   { id: 'Q1-H_evkDa4', order: 8, rubric: 'Междучасие', title: 'Извън училище', date: '2026-02-24' },
   { id: 'tja7Y8GcqVU', order: 9, rubric: 'SPECIAL', title: 'Трети март | История за освобождението', date: '2026-03-03' },
-  { id: '6VzfCV5J_L0', order: 10, rubric: 'Междучасие', title: 'Как да съчетаем фитнес, училище и работа', date: '2026-01-26' },
-  { id: 'M-ynZ1ijkJE', order: 11, rubric: 'Междучасие', title: 'С какво се занимаваме извън ученето?', date: '2026-03-06' },
+  { id: '6VzfCV5J_L0', order: 10, rubric: 'Междучасие', title: 'Как да съчетаем фитнес, училище и работа', date: '2026-03-06' },
+  { id: 'M-ynZ1ijkJE', order: 11, rubric: 'Междучасие', title: 'С какво се занимаваме извън ученето?', date: '2026-05-07' },
   { id: 'FI1tHW7d3A8', order: 12, rubric: 'Междучасие', title: 'Проблемите на тийнейджърите', date: '2026-05-29' },
   { id: '5jDmPg-gVBo', order: 14, rubric: 'Междучасие', title: 'Нов училищен STEM център & Втори интердисциплинарен турнир „Деца на науката"', date: '2026-06-06' },
 ];
